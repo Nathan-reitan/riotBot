40e8c01741aacc113ac7ec43f54e3a611ae32a1f
Nathan Reitan <nathanreitan@gmail.com>
2020-10-15 12:02:45 -0700

    package.json added

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 const Discord = require('discord.js');
 const config = require ('./config.json');
+const client = new Discord.Client();
+
+
+const helpEmbed = {
+  title: "riotBot help desk",
+  description: `
+  1. !ritoplz: will put list of commands into chat;
+ `
+}
+
+client.once('ready', ()=>{
+  console.log('Hardstuck Wood IV')
+});
+
+client.login(config.token);
